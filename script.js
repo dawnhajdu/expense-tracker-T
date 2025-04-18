73324aaaa1b7c2bd9d0240dcfcf971328fe1983d
Dawn Hajdu <dawn.hajdu@gmail.com>
2025-04-17 21:22:32 -1000

    Update minor corrections

--- script.js
+++ script.js
@@ -67,7 +67,7 @@ function addTransactionDOM(transaction) {
 function updateValues() {
     const amounts = transactions.map(transaction => transaction.amount);
 
-    const total = amounts.reduce((acc, item) =>(acc += item), 0).toFixed(2);
+    const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
     
     const income = amounts.filter(item => item > 0).reduce((acc, item) => (acc += item), 0).toFixed(2);
 
@@ -97,6 +97,7 @@ function init() {
     list.innerHTML = '';
 
     transactions.forEach(addTransactionDOM);
+    
     updateValues();
 }
 
